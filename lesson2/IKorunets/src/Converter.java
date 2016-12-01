a429f231f1015c67ca269cb10df1f0aef0e779d3
Igor Korunets <igor.korounets@gmail.com>
2016-12-01 02:35:03 +0200

    add comments

--- lesson2/IKorunets/src/Converter.java
+++ lesson2/IKorunets/src/Converter.java
@@ -5,14 +5,18 @@ public class Converter {
     System.out.println(s + "(2) = " + convert(s) + "(10)");
   }
   public static int convert(String binary) {
-    int dec = 0;
-    int length = binary.length() - 1;
+    int dec = 0;                      //результат - дес€тичное число
+    int length = binary.length() - 1; //вводим доп. переменную и отнимакм от количества символов в стороке единицу,
+                                      //потому как в данной задаче дл€ метода charAt нам нужно семь позиций,
+                                      //и перва€ должна начинатьс€ с нул€.
 
     for(int i = 0; i <= length; i++){
-      if(binary.charAt(i) == '1') {
-        dec += Math.pow(2, length - i);
+      if(binary.charAt(i) == '1') {     //на каждой итерации метод charAt выбирает числа из строки по пор€дку слева направо,
+                                        //и если число равн€етс€ единице,
+        dec += Math.pow(2, length - i); //то метод pow умножает двойку на значение переменной length минус значение позиции в стороке
+                                        //и прибавл€ет к результату
       }
     }
-    return dec;
+    return dec; //возвращает в вызывающий код результат
   }
 }
